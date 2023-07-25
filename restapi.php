d6ac74f59d99907974df17d0e46336ae0d36370e
mikespub <github@mikespub.net>
2023-07-25 20:06:28 +0200

    with rest api endpoint

--- restapi.php
+++ restapi.php
@@ -19,13 +19,8 @@ $config['cops_author_split_first_letter'] = '0';
 $config['cops_titles_split_first_letter'] = '0';
 
 $request = new Request();
+$apiHandler = new RestApi($request);
 
 header('Content-Type:application/json;charset=utf-8');
 
-$path = RestApi::getPathInfo($request);
-$params = RestApi::matchPathInfo($path, $request);
-$request = RestApi::setParams($params, $request);
-
-$output = json_encode(RestApi::getJson($request));
-
-echo RestApi::replaceLinks($output, $request);
+echo $apiHandler->getOutput();
