df63b6629457f8c754cd9e1d9d71ed7792b81a18
mikespub <github@mikespub.net>
2023-08-17 22:38:11 +0200

    add a few methods to zipfile classes

--- lib/Calibre/Book.php
+++ lib/Calibre/Book.php
@@ -16,6 +16,7 @@ use SebLucas\Cops\Output\Format;
 use SebLucas\Cops\Pages\Page;
 use SebLucas\EPubMeta\EPub;
 use SebLucas\TbsZip\clsTbsZip;
+//use SebLucas\EPubMeta\Tools\ZipEdit;
 use Exception;
 
 //class Book extends Base
@@ -498,7 +499,9 @@ class Book
         $data = $this->getDataById($idData);
 
         try {
+            // @todo try getting rid of this dependency here
             $epub = new EPub($data->getLocalPath(), clsTbsZip::class);
+            //$epub = new EPub($data->getLocalPath(), ZipEdit::class);
 
             $epub->setTitle($this->title);
             $authorArray = [];
