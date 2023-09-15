94c05bad6a37c00243493d729c535187fbee50ed
dunxd <d.drury@gmail.com>
2023-09-15 10:35:00 +0000

    Be consistant with semicolons at end of line

--- templates/bootstrap5/scripts/cops.js
+++ templates/bootstrap5/scripts/cops.js
@@ -1,6 +1,6 @@
 function postRefresh()
 {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
     hash = window.location.hash.replace("#", "");
     var elmnt = document.getElementById(hash);
