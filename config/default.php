a08d56cffc1b4ba85c8b8bec0c53188319ace885
mikespub <github@mikespub.net>
2025-11-02 13:08:07 +0100

    add cops_form_authentication for PR #161

--- config/default.php
+++ config/default.php
@@ -518,6 +518,17 @@ $config['calibre_user_database'] = null;
 $config['cops_basic_authentication'] = null;
 
 /*
+ * @todo see https://github.com/mikespub-org/seblucas-cops/pull/161 by @dcoffin88
+ * Enable form password protection via login.html (You can use if htpasswd is not possible for you)
+ * If possible prefer htpasswd or authentication done by reverse proxy!
+ * Supported values:
+ *     array with ["username" => "xxx", "password" => "secret"] : Enable form password protection
+ *     null : Disable form password protection (You can still use htpasswd or reverse proxy)
+ *     string with $config['calibre_user_database'] : Calibre user accounts database - WARNING: passwords are in clear!
+ */
+$config['cops_form_authentication'] = null;
+
+/*
  * Which template is used by default :
  * 'default'
  * 'bootstrap'
